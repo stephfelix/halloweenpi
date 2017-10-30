5b871337c1098fb528a54720f77cb68b34f115e8
Stéphane Félix <stephane.felix@gmail.com>
2017-10-29 21:57:03 -0400

    Added/Removed audio assets and images, added the clear function to the screen controller

--- config/config.js
+++ config/config.js
@@ -1,25 +1,24 @@
 module.exports = {
     soundFiles: [
+        './assets/cat-scream.mp3',
 		'./assets/cuckoo-clock.mp3',
-        './assets/foghorn-doorbell.mp3',
-        './assets/ghost-scream.mp3',
+        './assets/dalek-exterminate.mp3',
         './assets/evil-laugh.mp3',
         './assets/female-scream.mp3',
+        './assets/foghorn-doorbell.mp3',
         './assets/funeral-bells.mp3',
-        './assets/howling-wolf.mp3',
-        './assets/cat-scream.mp3',
-        './assets/woman-shrill-scream.mp3',
-        './assets/thriller-laugh.mp3',
-        './assets/glados-hello-friend.mp3',
-        './assets/glados-hello-where-are-you.mp3',
-        './assets/glados-i-see-you.mp3',
+        './assets/ghost-scream.mp3',
         './assets/glados-laugh.mp3',
-        './assets/glados-come-over-here.mp3',
-        './assets/dalek-exterminate.mp3'
+        './assets/thriller-laugh.mp3',
+        './assets/woman-shrill-scream.mp3',
+        './assets/thunder_strike_1-Mike_Koenig-739781745.mp3'
 	],
 
     images: [
-        './images/Yoda_SWSB.png',
-        './images/donald_trump_official_portrait.jpg'
+        './images/ghost.jpg',
+        './images/ghosts.jpg',
+        './images/110Drakelow_Tunnels.jpg',
+        './images/hqdefault.jpg'
+
     ]
 };
